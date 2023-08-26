e12a5f0787bab7e235cceda15573b567fbb634b7
kay-cee-web <ezekaval81@gmail.com>
2023-08-26 08:00:26 +0100

    initial

---
+++ app/news/latest/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function LatestNews() {
+  return (
+    <main>
+        <div>LatestNews</div>
+        
+    </main>
+  )
+}
---
+++ app/tickets/latest/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function latest() {
+  return (
+    <main>
+
+        <div>Latest Ticket</div>
+    </main>
+  )
+}
