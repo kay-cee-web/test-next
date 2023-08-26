e09d2091dac307398f2b16b99f765e2601191c4a
kay-cee-web <ezekaval81@gmail.com>
2023-08-26 08:56:10 +0100

    name change

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
 
 import './globals.css'
 import { Rubik } from 'next/font/google'
-import Navbar from './components/Navbar'
+import Navbar from './components/navbar'
 
 const bubik = Rubik({ subsets: ['latin'] })
 
